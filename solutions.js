27ebd5be20f541f4b693739f5d94daaa68ea75b9
Katie Monfore <krae.monfore@gmail.com>
2023-11-15 14:44:00 -0600

    completed 10 functions exercises

--- solutions.js
+++ solutions.js
@@ -21,3 +21,64 @@
  * concat
  */
 
+//Question 1
+function isTrue(input) {
+    return input === true;
+}
+
+//Question 2
+function isFalse(input) {
+    return input === false;
+}
+
+//Question 3
+function not(input) {
+   return !input;
+}
+
+//Question 4
+function addOne(input) {
+   input = Number(input)
+    return input +1
+}
+
+//Question 5
+function isEven(input) {
+    return input % 2 === 0
+
+}
+
+//Question 6
+function isIdentical (a, b) {
+    if (a === b) {
+        return true
+    } else{
+        return false
+    }
+
+
+}
+//Question 7
+function isEqual (inputOne, inputTwo) {
+    if (inputOne == inputTwo) {
+        return true
+    } else{
+        return false
+    }
+}
+
+//Question 8
+function or (inputAlpha, inputBeta) {
+    return (inputAlpha || inputBeta)
+}
+
+//Question 9
+function and (a, b) {
+    return (a && b)
+}
+
+//Question 10
+
+function concat (a, b) {
+    return String(a) + String(b);
+}
